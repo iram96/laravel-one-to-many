44fce0db9079f6a038032c7a862d16546558314b
iram96 <mariano.maselli@virgilio.it>
2022-03-30 18:51:16 +0200

    Added Category Model and coonected to Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,10 @@ class Post extends Model
     //
 
     protected $fillable = [
-        'title', 'post_content', 'image', 'slug'
+        'title', 'post_content', 'image', 'slug', 'category_id'
     ];
+
+    public function category(){
+        return $this->belongsTo('App\Models\Category');
+    }
 }
